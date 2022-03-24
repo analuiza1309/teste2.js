ae5733fd66b45fb468949ed32f727285974343a7
A PONTEW <you@example.com>
2022-03-24 19:10:41 -0300

    teste2

---
+++ app.js
@@ -0,0 +1,24 @@
+function idade(){
+
+    let idade= prompt ("Qual a sua Idade?")
+
+if (idade >=18 ){
+
+window. location.href= "https://www.netflix.com/br/browse/genre/34399";
+}
+
+else  if ( idade < 18 && idade >=15){
+    window .location.href = "https://jovemaprendizbrasil.com.br/";
+}
+
+else {
+
+    window .location.href= 'https://www.smartkids.com.br/'
+}
+
+
+
+
+
+
+}
